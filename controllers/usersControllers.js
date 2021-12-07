8ee3a37786d8c4cf6407cf105dae4f1d8c2f58db
all-an <all.ancapistan@gmail.com>
2021-12-07 18:48:57 -0300

    understanding requests

---
+++ controllers/usersControllers.js
@@ -0,0 +1,50 @@
+import { v4 as uuidv4 } from 'uuid';
+
+let users = [] 
+
+export const getUsers = (req, res) => {
+    console.log(users);
+    res.send(users);
+}
+
+export const createUser = (req, res) => {
+     
+    const user = req.body;
+    
+    //const userId = uuidv4();
+    //const userWithId = { ...user, id: uuidv4() }
+
+    users.push({ ...user, id: uuidv4() });
+    
+    res.send(`User with the name ${user.firstName} added.`);
+}
+
+export const getUserById = (req, res) => {
+    const {id} = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+
+    res.send(foundUser);
+}
+
+export const deleteUserById = (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User with the id ${id} deleted.`);
+}
+
+export const updateUserById = (req, res) => {
+    const { id } = req.params;
+    let { firstName, lastName, age } = req.body;
+
+    let user = users.find((user) => user.id === id);
+
+    if(firstName) user.firstName = firstName;
+    if(lastName) user.lastName = lastName;
+    if(age) user.age = age;
+
+    res.send(`User with the id ${id} updated.`);
+}
+
--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,18 @@
 import express from 'express';
 
+import { createUser, deleteUserById, getUsers, getUserById, updateUserById  } from '../controllers/usersControllers.js';
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "James",
-        lastName: "Gosling",
-        age: 25
-    },
-
-    {
-        firstName: "Brendan",
-        lastName: "Eich",
-        age: 19
-    } 
-] 
-
 //all routes in here are starting with /users
-router.get('/', (req, res) => {
-    console.log(users);
-    res.send(users);
-});
+router.get('/', getUsers);
 
-router.post('/', (req, res) => {
-    console.log('POST ROUTE REACHED');
-    
-    console.log(req.body);
-    //users.push()
-    
-    res.send('POST ROUTE REACHED');
-})
+router.post('/', createUser )
+
+router.get('/:id', getUserById)
+
+router.delete('/:id', deleteUserById)
+
+router.patch('/:id', updateUserById)
 
 export default router;

01d936b532e33e947a50458c7d3735d25a44ce58
all-an <all.ancapistan@gmail.com>
2021-12-07 16:24:42 -0300

    understanding requests

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,33 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: "James",
+        lastName: "Gosling",
+        age: 25
+    },
+
+    {
+        firstName: "Brendan",
+        lastName: "Eich",
+        age: 19
+    } 
+] 
+
 //all routes in here are starting with /users
 router.get('/', (req, res) => {
-    res.send('Hello');
+    console.log(users);
+    res.send(users);
 });
+
+router.post('/', (req, res) => {
+    console.log('POST ROUTE REACHED');
+    
+    console.log(req.body);
+    //users.push()
+    
+    res.send('POST ROUTE REACHED');
+})
 
 export default router;
